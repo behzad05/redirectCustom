16951f3f550634085c83c76aa7f4952ac7818a44
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-04 01:43:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-<script>
-
 favicon="https://storage.googleapis.com/msgsndr/TabqlvGhEdxHpg7xkw7X/media/66293c89389126609db70ebb.jpeg ";
 document.querySelector("link[rel=icon]").href = favicon;
-
-</script>
